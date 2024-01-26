e34b2611e0dcf912f1604b85c715f2fb46a31852
MihailoMartinovic <46078081+MihailoMartinovic@users.noreply.github.com>
2024-01-26 16:41:17 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@ release = '1.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    ...
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +27,5 @@ language = 'python'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 #html_static_path = ['_static']
